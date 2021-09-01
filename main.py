0d4a4f1818db5682b85e26585f17fb6ae65c7205
bichn <caleb.klab@gmail.com>
2021-09-01 15:26:44 -0500

    display surface

---
+++ main.py
@@ -0,0 +1,45 @@
+import sys
+import pygame
+from pygame.locals import *
+
+
+WINDOWWIDTH = 1024
+WINDOWHEIGHT = 768
+GRAY = (185, 185, 185)
+FPS = 60
+
+
+def main():
+    global FPSCLOCK, DISPLAYSURF, BASICFONT, BIGFONT
+
+    pygame.init()
+    DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
+    FPSCLOCK = pygame.time.Clock()
+    BASICFONT = pygame.font.Font('freesansbold.ttf', 18)
+    BIGFONT = pygame.font.Font('freesansbold.ttf', 100)
+    while True:
+        run_game()
+
+def run_game():
+    checkForQuit()
+
+    DISPLAYSURF.fill(GRAY)
+
+    FPSCLOCK.tick(FPS)
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+def checkForQuit():
+    for event in pygame.event.get(QUIT):  # get all the QUIT events
+        terminate()  # terminate if any QUIT events are present
+    for event in pygame.event.get(KEYUP):  # get all the KEYUP events
+        if event.key == K_ESCAPE:
+            terminate()  # terminate if the KEYUP event was for the Esc key
+        pygame.event.post(event)  # put the other KEYUP event objects back
+
+
+if __name__ == '__main__':
+    main()
