139cbe2d6d1f63461d6d0cec85e2cc2232beec62
dislocated-averstra <74801364+dislocated-averstra@users.noreply.github.com>
2021-12-16 20:30:24 -0700

    A little clean up and making the boxes look like cards.

--- cardgame/cards/GeneralCard.py
+++ cardgame/cards/GeneralCard.py
@@ -8,8 +8,7 @@ class GeneralCard(pygame.sprite.Sprite):
     def __init__(self, x_position, y_position):
         pygame.sprite.Sprite.__init__(self, self.containers)
 
-        self.image = pygame.Surface((32, 32))
-        pygame.draw.rect(self.image, colors.RED, (0, 0, 32, 32))
+        self.image = pygame.image.load('resources/BasicCard.png')
 
         self.rect = self.image.get_rect()
         self.rect.x = x_position
